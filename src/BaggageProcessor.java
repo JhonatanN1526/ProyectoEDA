27b9c41dd2425f448c39108cb0a38c081859c026
Diego23-06 <diegoalexandermm.02@gmail.com>
2026-01-24 10:54:08 -0500

    Pila de equipaje y simulacion

--- src/BaggageProcessor.java
+++ src/BaggageProcessor.java
@@ -1,20 +1,37 @@
+import java.io.PrintStream;
+
 public class BaggageProcessor {
 
-    public void processBaggage(Avion avion, int clock) {
-        Stack<String> stack = new ArrayStack<>();
+
+    public int processBaggage(Avion avion, int clock, PrintStream out) {
+        if (avion == null) {
+            throw new IllegalArgumentException("El avion no puede ser nulo");
+        }
+        PrintStream target = out == null ? System.out : out;
+
+        Stack<String> stack = new ArrayStack<>(ConfigSimulacion.CANTIDAD_EQUIPAJE);
 
         for (int i = 1; i <= ConfigSimulacion.CANTIDAD_EQUIPAJE; i++) {
             String containerLabel = "Avion " + avion.getId() + " contenedor #" + i;
             stack.push(containerLabel);
-            System.out.println("[t=" + clock + "] Apilado " + containerLabel);
+            target.println("[t=" + clock + "] Apilado " + containerLabel);
         }
 
         while (!stack.isEmpty()) {
             String container = stack.pop();
-            System.out.println("[t=" + clock + "] Procesado " + container);
+            target.println("[t=" + clock + "] Procesado " + container);
             clock += ConfigSimulacion.TIEMPO_POR_EQUIPAJE;
         }
 
-        System.out.println("Equipaje completado para " + avion + " en t=" + clock);
+        target.println("Equipaje completado para " + avion + " en t=" + clock);
+        return clock;
+    }
+
+    public int processBaggage(Avion avion, int clock) {
+        return processBaggage(avion, clock, System.out);
+    }
+
+    public int processBaggage(Avion avion) {
+        return processBaggage(avion, 0, System.out);
     }
 }
--- src/MainTestBaggage.java
+++ src/MainTestBaggage.java
@@ -1,10 +1,11 @@
 public class MainTestBaggage {
     public static void main(String[] args) {
-        // Prueba mínima: procesar equipaje de un avión
         Avion avion = new Avion("A123", false, 10);
         BaggageProcessor processor = new BaggageProcessor();
-        int clock = 0;
-        processor.processBaggage(avion, clock);
+
+        int startClock = 0;
+        int endClock = processor.processBaggage(avion, startClock);
+        System.out.println("Tiempo total transcurrido: " + (endClock - startClock) + " unidades");
     }
 }
 
